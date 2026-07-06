da51f062d9844133c755509b201bab04eb8f62c8
fpalomino-db <rozhokx123@gmail.com>
2026-07-05 20:26:24 -0500

    Vercel URL a Cors

--- PrevDengue/src/main/java/pe/edu/upc/prevdengue/util/CorsConfig.java
+++ PrevDengue/src/main/java/pe/edu/upc/prevdengue/util/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
         // 🚀 AQUÍ: Agregaremos tu URL de Vercel una vez que la plataforma te la dé.
         // Por ahora lo dejamos con localhost, y cuando tengas Vercel será algo como:
         // config.setAllowedOrigins(List.of("http://localhost:4200", "https://prevdengue-frontend.vercel.app"));
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of("http://localhost:4200","https://prevdengue-frontend.vercel.app"));
 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
